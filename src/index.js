f3acde66f78d07431cdec969c6af0ca78a626660
CarinaLR <carinalr84@gmail.com>
2019-09-30 12:53:58 -0400

    Make factory Module

---
+++ src/index.js
@@ -0,0 +1,24 @@
+console.log('Afia')
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWZpYXNpYXciLCJhIjoiY2sxNmttdmQ3MHlmaDNsbml0bjBzYzVmaSJ9.U_VhavXfnJI3IixgJXeZPA';
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009151, 40.705086]).addTo(map);
+
+// const buildMarker = require("./marker");
+
+// const marker = buildMarker("hotels", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
+// marker.addTo(map);
